9b1dadf8ccf37335ffb86563693c8fffa8c94259
imkrishndev <imkrishndev@gmail.com>
2026-04-28 13:53:29 +0530

    User Create issue resolved

--- addons19/oh_employee_creation_from_user/models/res_users.py
+++ addons19/oh_employee_creation_from_user/models/res_users.py
@@ -1,46 +1,37 @@
-# -*- coding: utf-8 -*-
-#############################################################################
-#    A part of Open HRMS Project <https://www.openhrms.com>
-#
-#    Cybrosys Technologies Pvt. Ltd.
-#
-#    Copyright (C) 2025-TODAY Cybrosys Technologies(<https://www.cybrosys.com>)
-#    Author: Cybrosys Techno Solutions(<https://www.cybrosys.com>)
-#
-#    You can modify it under the terms of the GNU LESSER
-#    GENERAL PUBLIC LICENSE (LGPL v3), Version 3.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU LESSER GENERAL PUBLIC LICENSE (LGPL v3) for more details.
-#
-#    You should have received a copy of the GNU LESSER GENERAL PUBLIC LICENSE
-#    (LGPL v3) along with this program.
-#    If not, see <http://www.gnu.org/licenses/>.
-#
-#############################################################################
 from odoo import api, fields, models
 
 
 class ResUsers(models.Model):
-    """ Inherited class of res user to override the create function"""
     _inherit = 'res.users'
 
-    employee_id = fields.Many2one('hr.employee',
-                                  string='Related Employee',
-                                  ondelete='restrict', auto_join=True,
-                                  help='Related employee based on the'
-                                       ' data of the user')
+    employee_id = fields.Many2one(
+        'hr.employee',
+        string='Related Employee',
+        ondelete='restrict',
+        auto_join=True
+    )
 
     @api.model_create_multi
-    def create(self, vals):
-        """Overrides the default 'create' method to create an employee record
-        when a new user is created."""
-        result = super(ResUsers, self).create(vals)
-        result['employee_id'] = self.env['hr.employee'].sudo().create({
-            'name': result['name'],
-            'user_id': result['id'],
-            'private_street': result['partner_id'].id
-        })
-        return result
+    def create(self, vals_list):
+        users = super().create(vals_list)
+
+        for user in users:
+            # ✅ Check if employee already exists
+            existing_employee = self.env['hr.employee'].sudo().search([
+                ('user_id', '=', user.id),
+                ('company_id', '=', user.company_id.id)
+            ], limit=1)
+
+            if not existing_employee:
+                employee = self.env['hr.employee'].sudo().create({
+                    'name': user.name,
+                    'user_id': user.id,
+                    'work_email': user.login,
+                    'company_id': user.company_id.id,
+                })
+                user.employee_id = employee.id
+            else:
+                # ✅ Just link if already exists
+                user.employee_id = existing_employee.id
+
+        return users
