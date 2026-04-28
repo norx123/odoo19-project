51a5a278b46a51abd3738befc8a1cc2fd718bf2f
imkrishndev <imkrishndev@gmail.com>
2026-04-28 11:25:40 +0530

    New All modules updated

---
+++ addons19/employee_tax_sheet_module/__manifest__.py
@@ -0,0 +1,9 @@
+{
+    'name': 'Employee Tax Sheet',
+    'version': '1.0',
+    'depends': ['hr'],
+    'category': 'Human Resources',
+    'author': 'Krishn Dev',
+    'data': ['views/hr_employee_tax_sheet.xml'],
+    'installable': True
+}
